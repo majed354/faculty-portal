192db0f8fef15d44382135227543b92e7a5fcde3
majed354 <majed1427h@gmail.com>
2025-11-05 02:10:33 +0300

    Delete src directory

--- src/lib/types.ts
+++
@@ -1,3 +0,0 @@
-export type TermId = string; // مثل: 2024-fall أو 1446-1
-export type Rank = 'LECTURER' | 'ASSISTANT_PROF' | 'ASSOCIATE_PROF' | 'PROFESSOR';
-export type Nationality = 'SAUDI' | 'FOREIGN';
